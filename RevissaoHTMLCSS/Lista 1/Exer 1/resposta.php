189c4b194fb49a310c3dd3e511f4ebcfb82c2c30
thamires-caetano <99516058+thamires-caetano@users.noreply.github.com>
2022-04-13 21:18:46 -0300

    lista 1

---
+++ RevissaoHTMLCSS/Lista 1/Exer 1/resposta.php
@@ -0,0 +1,47 @@
+<?php
+ //print_r($_POST) - exibindo o conteúdo do array
+ //echo $_POST["valor_pago"] - exibe um valor na tela
+
+ $valor_pago = $_POST['valor_pago'];
+ $valor_total = $_POST['valor_total'];
+
+ if ($valor_total == $valor_pago)
+    $resultado = "Não há troco!";
+ elseif ($valor_pago < $valor_total)
+    $resultado = "Valor pago insuficiente!";
+ else
+    $resultado = "O troco é de: ".($valor_pago - $valor_total);
+?>
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+
+    <title>Exercicio 1 - Resultado</title>
+  </head>
+  <body class="container">
+    <div id="box-valores">
+      <h2 class="mt-5 display-6"><?php echo $resultado?></h2>
+    </div>
+
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
